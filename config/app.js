cdc8d40a1b480fd0a61265c612a3cb544fab9a0e
pansay <jan.krepelka@gmail.com>
2015-10-06 20:22:53 +0200

    files relative path

--- config/app.js
+++ config/app.js
@@ -1,4 +1,4 @@
 module.exports = {
     "apiUrl": "https://api.github.com/repos/thecivilized/thecivilized/contents/",
-    "apiUrlRaw": "https://raw.githubusercontent.com/thecivilized/thecivilized/master/"
+    "apiUrlRaw": "/"
 };
